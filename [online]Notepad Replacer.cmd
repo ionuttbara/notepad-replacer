c58177c1b99f0ba3a4d3b5300b0d3bd12b01da8a
geoni <76656855+jbara2002@users.noreply.github.com>
2022-06-04 15:39:27 +0300

    Delete [online]Notepad Replacer.cmd

--- [online]Notepad Replacer.cmd
+++
@@ -1,7 +0,0 @@
-:: Notepad Replacer with Fidelity Package Manager
-choco install notepadplusplus -y
-choco install notepadreplacer -y --params='"/NOTEPAD:C:\Program Files\Notepad++\notepad++.exe"'
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe\0" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe\1" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe\2" /f
-pause
